0ee6469b8f1610a85781b1b4b071c8e31d456ab6
dkwapisz <dkwapisz99@gmail.com>
2025-02-22 21:29:25 +0100

    Bring back phase5

--- data_processing/ColorSpaceConverter.py
+++ data_processing/ColorSpaceConverter.py
@@ -8,49 +8,15 @@ def normalize_zscore(image):
   return (image - mean) / std
 
 # Normal Phase1-5
-# def perform_basic_transformation(image, color_space):
-#     if "RGB" in color_space:
-#         return image
-#     elif "HSV" in color_space:
-#         return tf.image.rgb_to_hsv(image)
-#     elif "YUV" in color_space:
-#         return tf.image.rgb_to_yuv(image)
-#     elif "YIQ" in color_space:
-#         return tf.image.rgb_to_yiq(image)
-#     elif "CMYK" in color_space:
-#         cmy = 1 - image
-#         k = tf.reduce_min(cmy, axis=-1, keepdims=True)
-#         denominator = 1 - k + 1e-8
-#         c = (cmy[..., 0:1] - k) / denominator
-#         m = (cmy[..., 1:2] - k) / denominator
-#         y = (cmy[..., 2:3] - k) / denominator
-#         return tf.concat([c, m, y, k], axis=-1)
-
-# Phase6
 def perform_basic_transformation(image, color_space):
     if "RGB" in color_space:
-        r, g, b = tf.split(image, 3, axis=-1)
-        magnitude = tf.sqrt(r ** 2 + g ** 2 + b ** 2)
-        phase_rg = tf.atan2(g, r)
-        phase_rb = tf.atan2(b, r)
-        return tf.concat([tf.math.log1p(magnitude), phase_rg, phase_rb, magnitude * tf.cos(phase_rg + phase_rb)], axis=-1)
+        return image
     elif "HSV" in color_space:
-        image = tf.image.rgb_to_hsv(image)
-        h, s, v = tf.split(image, 3, axis=-1)
-        theta = h * 2 * np.pi
-        return tf.concat([v, s * tf.cos(theta), s * tf.sin(theta), v * tf.cos(theta)], axis=-1)
+        return tf.image.rgb_to_hsv(image)
     elif "YUV" in color_space:
-        image = tf.image.rgb_to_yuv(image)
-        y, u, v = tf.split(image, 3, axis=-1)
-        magnitude = tf.sqrt(u ** 2 + v ** 2)
-        theta = tf.atan2(v, u)
-        return tf.concat([y, magnitude * tf.cos(2 * theta), magnitude * tf.sin(2 * theta), tf.exp(-magnitude)], axis=-1)
+        return tf.image.rgb_to_yuv(image)
     elif "YIQ" in color_space:
-        image = tf.image.rgb_to_yiq(image)
-        y, i, q = tf.split(image, 3, axis=-1)
-        magnitude = tf.sqrt(i ** 2 + q ** 2)
-        theta = tf.atan2(q, i)
-        return tf.concat([y, magnitude * tf.cos(2 * theta), magnitude * tf.sin(2 * theta), tf.exp(-magnitude)], axis=-1)
+        return tf.image.rgb_to_yiq(image)
     elif "CMYK" in color_space:
         cmy = 1 - image
         k = tf.reduce_min(cmy, axis=-1, keepdims=True)
@@ -59,6 +25,40 @@ def perform_basic_transformation(image, color_space):
         m = (cmy[..., 1:2] - k) / denominator
         y = (cmy[..., 2:3] - k) / denominator
         return tf.concat([c, m, y, k], axis=-1)
+
+# Phase6
+# def perform_basic_transformation(image, color_space):
+#     if "RGB" in color_space:
+#         r, g, b = tf.split(image, 3, axis=-1)
+#         magnitude = tf.sqrt(r ** 2 + g ** 2 + b ** 2)
+#         phase_rg = tf.atan2(g, r)
+#         phase_rb = tf.atan2(b, r)
+#         return tf.concat([tf.math.log1p(magnitude), phase_rg, phase_rb, magnitude * tf.cos(phase_rg + phase_rb)], axis=-1)
+#     elif "HSV" in color_space:
+#         image = tf.image.rgb_to_hsv(image)
+#         h, s, v = tf.split(image, 3, axis=-1)
+#         theta = h * 2 * np.pi
+#         return tf.concat([v, s * tf.cos(theta), s * tf.sin(theta), v * tf.cos(theta)], axis=-1)
+#     elif "YUV" in color_space:
+#         image = tf.image.rgb_to_yuv(image)
+#         y, u, v = tf.split(image, 3, axis=-1)
+#         magnitude = tf.sqrt(u ** 2 + v ** 2)
+#         theta = tf.atan2(v, u)
+#         return tf.concat([y, magnitude * tf.cos(2 * theta), magnitude * tf.sin(2 * theta), tf.exp(-magnitude)], axis=-1)
+#     elif "YIQ" in color_space:
+#         image = tf.image.rgb_to_yiq(image)
+#         y, i, q = tf.split(image, 3, axis=-1)
+#         magnitude = tf.sqrt(i ** 2 + q ** 2)
+#         theta = tf.atan2(q, i)
+#         return tf.concat([y, magnitude * tf.cos(2 * theta), magnitude * tf.sin(2 * theta), tf.exp(-magnitude)], axis=-1)
+#     elif "CMYK" in color_space:
+#         cmy = 1 - image
+#         k = tf.reduce_min(cmy, axis=-1, keepdims=True)
+#         denominator = 1 - k + 1e-8
+#         c = (cmy[..., 0:1] - k) / denominator
+#         m = (cmy[..., 1:2] - k) / denominator
+#         y = (cmy[..., 2:3] - k) / denominator
+#         return tf.concat([c, m, y, k], axis=-1)
 
 def perform_hypercomplex_transformation(image, color_space):
     if "RGB" in color_space:
