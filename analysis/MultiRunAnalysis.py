d0d5d3db64d970385febad76e6e44dc0fd8ae50b
dkwapisz <dkwapisz99@gmail.com>
2025-03-08 23:24:51 +0100

    Phase 10 + results for phase 9

--- analysis/MultiRunAnalysis.py
+++ analysis/MultiRunAnalysis.py
@@ -178,7 +178,8 @@ def plot_multirun_phase_results(phase_data, phase_number, run_labels, one_type_o
 # phase5_data = read_phase_data(PHASE_5, RUNS_TO_TEST, only_hypercomplex=False)
 # phase6_data = read_phase_data(PHASE_6, RUNS_TO_TEST, only_hypercomplex=False)
 # phase7_data = read_phase_data(PHASE_7, RUNS_TO_TEST, only_hypercomplex=False)
-phase8_data = read_phase_data(PHASE_8, RUNS_TO_TEST, only_hypercomplex=False)
+# phase8_data = read_phase_data(PHASE_8, RUNS_TO_TEST, only_hypercomplex=False)
+phase9_data = read_phase_data(PHASE_9, RUNS_TO_TEST, only_hypercomplex=False)
 
 # plot_multirun_phase_results(phase1_data, PHASE_1, RUN_LABELS_SPLIT_1234)
 # plot_multirun_phase_results(phase2_data, PHASE_2, RUN_LABELS_SPLIT_1234)
@@ -187,7 +188,8 @@ phase8_data = read_phase_data(PHASE_8, RUNS_TO_TEST, only_hypercomplex=False)
 # plot_multirun_phase_results(phase5_data, PHASE_5, RUN_LABELS_SPLIT_5_PLUS, one_type_only=True) # Only CNN models
 # plot_multirun_phase_results(phase6_data, PHASE_6, RUN_LABELS_SPLIT_5_PLUS, one_type_only=True) # Only CNN models
 # plot_multirun_phase_results(phase7_data, PHASE_7, RUN_LABELS_SPLIT_5_PLUS)
-plot_multirun_phase_results(phase8_data, PHASE_8, RUN_LABELS_SPLIT_5_PLUS)
+# plot_multirun_phase_results(phase8_data, PHASE_8, RUN_LABELS_SPLIT_5_PLUS)
+plot_multirun_phase_results(phase9_data, PHASE_9, RUN_LABELS_SPLIT_5_PLUS)
 
 # # ------------------- Phase 1 vs Phase 2 -------------------
 # get_average_evaluation_between_phases_grouped_by(phase1_data, phase2_data, (1, 2), RUN_LABELS_SPLIT_1234,  RUNS_TO_TEST, "color_space", "accuracy")
