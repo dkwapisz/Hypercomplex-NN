ea75e4941551d664d505fff21880b2090cb76523
dkwapisz <dkwapisz99@gmail.com>
2025-03-13 21:31:53 +0100

    Mean average accuracy per model (phase8-14)

--- analysis/ResultEvaluation.py
+++ analysis/ResultEvaluation.py
@@ -31,6 +31,13 @@ def get_average_evaluation_between_phases_by_model(phases_data, phase_nums, run_
     with open(f"between_phases_results/phase{'_'.join(map(str, phase_nums))}_by_average_{evaluation_key}.md", "w") as f:
         f.write(markdown_table)
 
+    model_averages = df.groupby("model")[f"average_{evaluation_key}"].mean().reset_index().sort_values(by=[f"average_{evaluation_key}"], ascending=False, inplace=False)
+    model_averages.columns = ["model", f"mean_average_{evaluation_key}"]
+
+    model_averages_markdown = model_averages.to_markdown(index=False)
+    with open(f"between_phases_results/mean_average_{evaluation_key}_per_model.md", "w") as f:
+        f.write(model_averages_markdown)
+
 
 get_average_evaluation_between_phases_by_model(phases_to_compare, (8, 9, 10, 11, 12, 13, 14), RUN_LABELS_SPLIT_5_PLUS, RUNS_TO_TEST, "accuracy")
 get_average_evaluation_between_phases_by_model(phases_to_compare, (8, 9, 10, 11, 12, 13, 14), RUN_LABELS_SPLIT_5_PLUS, RUNS_TO_TEST, "F1-Score")
---
+++ analysis/between_phases_results/mean_average_F1-Score_per_model.md
@@ -0,0 +1,10 @@
+| model                          |   mean_average_F1-Score |
+|:-------------------------------|------------------------:|
+| HyperComplex-YUV-Bicomplex     |                0.842838 |
+| HyperComplex-YUV-Quaternions   |                0.836214 |
+| CNN-YUV                        |                0.828168 |
+| HyperComplex-RGB-Cl11          |                0.825952 |
+| HyperComplex-RGB-Cl20          |                0.823273 |
+| HyperComplex-YUV-Coquaternions |                0.817644 |
+| HyperComplex-YUV-Tessarines    |                0.808232 |
+| CNN-RGB                        |                0.779491 |
