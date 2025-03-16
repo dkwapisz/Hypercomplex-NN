1d46db7f0bc70ede4fb05dddc3de270d33b8a732
dkwapisz <dkwapisz99@gmail.com>
2025-03-16 20:45:11 +0100

    Run2 - CNN-YUV and HCNN-YUV-Quaternions - Testing tuned models

--- models/CNN_Model.py
+++ models/CNN_Model.py
@@ -32,22 +32,20 @@ def build_model(input_shape, num_classes, metrics):
     model = Sequential()
     model.add(Input(shape=input_shape))
 
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D())
-
-    model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D())
+    model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D(strides=2))
 
     model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D())
+    model.add(MaxPooling2D(strides=2))
 
-    model.add(Conv2D(8, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D())
 
     model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))
     model.add(MaxPooling2D())
 
-    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D())
+    model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D(strides=2))
 
     model.add(Flatten())
     model.add(Dense(num_classes, activation='softmax'))
