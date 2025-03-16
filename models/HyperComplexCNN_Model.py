5493bf2208c1eddd6a0d3cd6cde54bf5f78422a6
dkwapisz <dkwapisz99@gmail.com>
2025-03-16 21:17:44 +0100

    Run3 - (70/20/10)

--- models/HyperComplexCNN_Model.py
+++ models/HyperComplexCNN_Model.py
@@ -34,20 +34,17 @@ def build_model(input_shape, num_classes, metrics, algebra):
     model = Sequential()
     model.add(Input(shape=input_shape))
 
-    model.add(HyperConv2D(8, (5, 5), padding='SAME', activation='relu', algebra=algebra))
-    model.add(MaxPooling2D(strides=2))
-
-    model.add(HyperConv2D(32, (3, 3), padding='SAME', activation='relu', algebra=algebra))
-    model.add(MaxPooling2D(strides=2))
-
-    model.add(HyperConv2D(64, (3, 3), padding='SAME', activation='relu', algebra=algebra))
-    model.add(MaxPooling2D())
-
-    model.add(HyperConv2D(8, (5, 5), padding='SAME', activation='relu', algebra=algebra))
+    model.add(HyperConv2D(16, (3, 3), padding='SAME', activation='relu', algebra=algebra))
     model.add(MaxPooling2D())
 
     model.add(HyperConv2D(16, (5, 5), padding='SAME', activation='relu', algebra=algebra))
-    model.add(MaxPooling2D(strides=2))
+    model.add(MaxPooling2D())
+
+    model.add(HyperConv2D(64, (5, 5), padding='SAME', activation='relu', algebra=algebra))
+    model.add(MaxPooling2D())
+
+    model.add(HyperConv2D(32, (3, 3), padding='SAME', activation='relu', algebra=algebra))
+    model.add(MaxPooling2D())
 
     model.add(Flatten())
     model.add(Dense(num_classes, activation='softmax'))
