0e71e52380e6d1e005fe7e6f86d46ec85f4ddbeb
dkwapisz <dkwapisz99@gmail.com>
2025-03-16 20:42:16 +0100

    Run2 - CNN-RGB and HCNN-YUV-Bicomplex - Testing tuned models

--- models/CNN_Model.py
+++ models/CNN_Model.py
@@ -32,22 +32,22 @@ def build_model(input_shape, num_classes, metrics):
     model = Sequential()
     model.add(Input(shape=input_shape))
 
-    model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D(strides=2))
-
     model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-    model.add(MaxPooling2D())
-
-    model.add(Conv2D(256, (7, 7), padding='same', activation='relu'))
-    model.add(MaxPooling2D(strides=2))
-
-    model.add(Conv2D(128, (5, 5), padding='same', activation='relu'))
     model.add(MaxPooling2D())
 
     model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D())
 
-    model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(8, (3, 3), padding='same', activation='relu'))
+
+    model.add(Conv2D(32, (5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(MaxPooling2D())
 
     model.add(Flatten())
     model.add(Dense(num_classes, activation='softmax'))
--- models/HyperComplexCNN_Model.py
+++ models/HyperComplexCNN_Model.py
@@ -34,14 +34,16 @@ def build_model(input_shape, num_classes, metrics, algebra):
     model = Sequential()
     model.add(Input(shape=input_shape))
 
-    model.add(HyperConv2D(32, (3, 3), padding='SAME', activation='relu', algebra=algebra))
-    model.add(MaxPooling2D())
-
-    model.add(HyperConv2D(32, (7, 7), padding='SAME', activation='relu', algebra=algebra))
-    model.add(MaxPooling2D(strides=2))
-
     model.add(HyperConv2D(64, (3, 3), padding='SAME', activation='relu', algebra=algebra))
     model.add(MaxPooling2D())
+
+    model.add(HyperConv2D(64, (5, 5), padding='SAME', activation='relu', algebra=algebra))
+    model.add(MaxPooling2D())
+
+    model.add(HyperConv2D(64, (5, 5), padding='SAME', activation='relu', algebra=algebra))
+    model.add(MaxPooling2D())
+
+    model.add(HyperConv2D(64, (7, 7), padding='SAME', activation='relu', algebra=algebra))
 
     model.add(Flatten())
     model.add(Dense(num_classes, activation='softmax'))
