d16054b40f0b10e404c3fa4b97ad100358824061
dkwapisz <dkwapisz99@gmail.com>
2025-03-17 22:23:10 +0100

    test permutations for YUV and HCNN - phase21

--- analysis/SingleRunAnalysis.py
+++ analysis/SingleRunAnalysis.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # ------------------- Static parameters -------------------
 start_phase = 19        # Inclusive
-end_phase = 19          # Inclusive
+end_phase = 20          # Inclusive
 run_phases = [f"phase{i}" for i in range(start_phase, end_phase + 1)]
 runs = ["run1", "run2", "run3", "run4", "run5", "run6", "run7", "run8", "run9", "run10"]
 json_result_file = "training.json"
--- data_processing/ColorSpaceConverter.py
+++ data_processing/ColorSpaceConverter.py
@@ -69,11 +69,7 @@ def perform_transformation_for_hypercomplex(image, color_space):
         theta = h * 2 * np.pi
         return tf.concat([v, s * tf.cos(theta), s * tf.sin(theta), v * tf.cos(theta)], axis=-1)
     elif "YUV" in color_space:
-        image = tf.image.rgb_to_yuv(image)
-        y, u, v = tf.split(image, 3, axis=-1)
-        magnitude = tf.sqrt(u ** 2 + v ** 2)
-        theta = tf.atan2(v, u)
-        return tf.concat([y, magnitude * tf.cos(2 * theta), magnitude * tf.sin(2 * theta), tf.exp(-magnitude)], axis=-1)
+        return yuv_permutation(color_space, image)
     elif "YIQ" in color_space:
         image = tf.image.rgb_to_yiq(image)
         y, i, q = tf.split(image, 3, axis=-1)
@@ -88,6 +84,59 @@ def perform_transformation_for_hypercomplex(image, color_space):
         m = (cmy[..., 1:2] - k) / denominator
         y = (cmy[..., 2:3] - k) / denominator
         return tf.concat([c, m, y, k], axis=-1)
+
+def yuv_permutation(color_space, image):
+    image = tf.image.rgb_to_yuv(image)
+    y, u, v = tf.split(image, 3, axis=-1)
+
+    if color_space == "YUV-1":
+        return tf.concat([y, u, v, 0], axis=-1)
+    elif color_space == "YUV-2":
+        return tf.concat([y, u, 0, v], axis=-1)
+    elif color_space == "YUV-3":
+        return tf.concat([y, v, u, 0], axis=-1)
+    elif color_space == "YUV-4":
+        return tf.concat([y, v, 0, u], axis=-1)
+    elif color_space == "YUV-5":
+        return tf.concat([y, 0, u, v], axis=-1)
+    elif color_space == "YUV-6":
+        return tf.concat([y, 0, v, u], axis=-1)
+    elif color_space == "YUV-7":
+        return tf.concat([u, y, v, 0], axis=-1)
+    elif color_space == "YUV-8":
+        return tf.concat([u, y, 0, v], axis=-1)
+    elif color_space == "YUV-9":
+        return tf.concat([u, v, y, 0], axis=-1)
+    elif color_space == "YUV-10":
+        return tf.concat([u, v, 0, y], axis=-1)
+    elif color_space == "YUV-11":
+        return tf.concat([u, 0, y, v], axis=-1)
+    elif color_space == "YUV-12":
+        return tf.concat([u, 0, v, y], axis=-1)
+    elif color_space == "YUV-13":
+        return tf.concat([v, y, u, 0], axis=-1)
+    elif color_space == "YUV-14":
+        return tf.concat([v, y, 0, u], axis=-1)
+    elif color_space == "YUV-15":
+        return tf.concat([v, u, y, 0], axis=-1)
+    elif color_space == "YUV-16":
+        return tf.concat([v, u, 0, y], axis=-1)
+    elif color_space == "YUV-17":
+        return tf.concat([v, 0, y, u], axis=-1)
+    elif color_space == "YUV-18":
+        return tf.concat([v, 0, u, y], axis=-1)
+    elif color_space == "YUV-19":
+        return tf.concat([0, y, u, v], axis=-1)
+    elif color_space == "YUV-20":
+        return tf.concat([0, y, v, u], axis=-1)
+    elif color_space == "YUV-21":
+        return tf.concat([0, u, y, v], axis=-1)
+    elif color_space == "YUV-22":
+        return tf.concat([0, u, v, y], axis=-1)
+    elif color_space == "YUV-23":
+        return tf.concat([0, v, y, u], axis=-1)
+    elif color_space == "YUV-24":
+        return tf.concat([0, v, u, y], axis=-1)
 
 def convert_color_tf(image, color_space, hypercomplex=False):
     image = tf.image.convert_image_dtype(image, tf.float32)
