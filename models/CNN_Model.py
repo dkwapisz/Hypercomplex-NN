5ab7542ddbf504a251b47e5ea0e99889143a5763
dkwapisz <dkwapisz99@gmail.com>
2025-04-20 18:52:37 +0200

    Rerun phase23-25 for AVG+STD analysis

--- models/CNN_Model.py
+++ models/CNN_Model.py
@@ -32,13 +32,16 @@ def build_model(input_shape, num_classes, metrics):
     model = Sequential()
     model.add(Input(shape=input_shape))
 
-    model.add(Conv2D(16, (3, 3), activation='relu'))
-    model.add(MaxPooling2D())
-
     model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D())
 
     model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D())
+
+    model.add(Conv2D(256, (5, 5), activation='relu'))
     model.add(MaxPooling2D())
 
     model.add(Flatten())
---
+++ statistic_phase_runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in {1..5}; do
+  echo "----------------- STARTING ITERATION $i -----------------"
+
+  ./phase_runner.sh
+
+  if [ $? -ne 0 ]; then
+    echo "Error occurred during iteration $i. Exiting..."
+    exit 1
+  fi
+
+
+  zip -r final_results$((i+1)).zip final_results
+  echo "----------------- FINISHED ITERATION $i -----------------"
+done
+
+echo "All iterations completed successfully."
